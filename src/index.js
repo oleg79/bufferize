4fafc8c4a95b2d4511f66ca09b20791177626e72
oleg79 <0112oleg@gmail.com>
2018-07-21 01:23:14 +0300

    ADDED: tests, gitignore

---
+++ src/index.js
@@ -0,0 +1,31 @@
+export default (callingFunction, time, releaseFunction) => {
+  let buffer = []
+  let isBuffering = false
+  let timerId
+
+  return {
+    fire(...args) {
+      buffer.push(callingFunction(...args))
+
+      if (releaseFunction && !isBuffering) {
+        isBuffering = true
+
+        timerId = setTimeout(() => {
+          releaseFunction(buffer.splice(0))
+          isBuffering = false
+          clearTimeout(timerId)
+        }, time)
+      }
+    },
+
+    release() {
+      const output = buffer.splice(0)
+      clearTimeout(timerId)
+      return releaseFunction ? releaseFunction(output) : output
+    },
+
+    latest() {
+      return buffer[ buffer.length - 1 ];
+    }
+  }
+}
---
+++ src/index.test.js
@@ -0,0 +1,92 @@
+import bufferize from '.'
+
+const callingFunction = x => x * x
+
+it('should return a proper array of invocation arguments', done => {
+  const releaseFunction = args => {
+    expect(args).toEqual([1,4,9,16])
+    done()
+  }
+
+  const buffered = bufferize(callingFunction, 100, releaseFunction)
+
+  buffered.fire(1)
+  buffered.fire(2)
+  buffered.fire(3)
+  buffered.fire(4)
+
+})
+
+
+
+it('should return a proper latest result of the invocation', () => {
+  const buffered = bufferize(callingFunction, 100, () => {})
+
+  buffered.fire(1)
+  expect(buffered.latest()).toBe(1)
+
+  buffered.fire(2)
+  expect(buffered.latest()).toBe(4)
+
+  buffered.fire(3)
+  expect(buffered.latest()).toBe(9)
+
+  buffered.fire(4)
+  expect(buffered.latest()).toBe(16)
+})
+
+
+
+it('should stop bufferizing after beforehand release', done => {
+  const releaseFunction = args => {
+    expect(args).toEqual([1,4,9,16])
+    done()
+    return args;
+  }
+
+  const buffered = bufferize(callingFunction, 100, releaseFunction)
+
+  buffered.fire(1)
+  buffered.fire(2)
+  buffered.fire(3)
+  buffered.fire(4)
+
+  buffered.release();
+
+  buffered.fire(5)
+})
+
+
+
+it('should return buffer on beforehand release', () => {
+  const releaseFunction = args => args
+
+  const buffered = bufferize(callingFunction, 100, releaseFunction)
+
+  buffered.fire(1)
+  buffered.fire(2)
+  buffered.fire(3)
+  buffered.fire(4)
+  const result = buffered.release();
+
+  expect(result).toEqual([1,4,9,16])
+})
+
+
+it('should stop bufferization after the expiration', done => {
+  const releaseFunction = args => {
+    expect(args).toEqual([1, 4, 9, 16])
+    done()
+  }
+
+  const buffered = bufferize(callingFunction, 0, releaseFunction)
+
+  setTimeout(() => {
+    buffered.fire(5)
+  }, 200)
+
+  buffered.fire(1)
+  buffered.fire(2)
+  buffered.fire(3)
+  buffered.fire(4)
+})
